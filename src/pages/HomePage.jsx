ed568c488d470742d98569df595d1ea3e66c4d96
simonelupone <simone.lupone@gmail.com>
2025-06-15 03:05:40 +0200

    feat: add homepage with videogames loop

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import Hero from "../components/Hero";
+import axios from "axios";
+
+const HomePage = () => {
+  const [videogames, setVideogames] = useState([]);
+  console.log(videogames);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/videogames/").then((response) => {
+      setVideogames(response.data.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Hero />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="">
+              {videogames.map((videogame) => (
+                <div key={videogame.id} className="card my-4">
+                  <div className="card-body">
+                    <div className="card-title fw-bold">{videogame.name}</div>
+                    <div className="card-text">{`${videogame.price}€`}</div>
+                    <div className="card-text">{videogame.publisher.name}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;

d1de80570c3e73dd030b0b58fca123c468441c37
simonelupone <simone.lupone@gmail.com>
2025-06-15 03:05:22 +0200

    feat: add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar bg-primary">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            <div className="d-flex align-items-center">
+              {/* logo */}
+              <span className="p-2 text-white fs-3">BOOGAMING</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

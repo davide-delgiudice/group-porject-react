4aac6e88afd7534aa2019ab737128a90557fc2da
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-17 16:39:00 +0200

    [UPD] create Publisher Section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="container py-5">
         <div className="row">
           <div className="col-12 col-md-10 col-lg-8">
-            <div className="">
+            <div>
               <h2 className="fw-bold">I TUOI VIDEOGIOCHI</h2>
               <h2 className="fw-bold">LA TUA STORIA</h2>
               <p className="mt-3">Boogaming è dove puoi trovare i tuoi giochi preferiti, 
---
+++ src/components/PublisherSection.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+
+const PublisherSection = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-12 col-md-4">
+                <h3>Best Publisher</h3>
+                <div className="card">
+                    <img src="" alt="..." />
+                    <div>
+                        <h5></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PublisherSection

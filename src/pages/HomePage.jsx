f6cb98db3ff11bf9b0883c2b604aa2f464a2ba0e
brian-tura <b.tura2002@gmail.com>
2025-06-17 13:49:31 +0200

    [UPD] Linked videogame cards to product page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Hero from "../components/Hero";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const [videogames, setVideogames] = useState([]);
@@ -21,24 +22,26 @@ const HomePage = () => {
           <div className="col-12">
             <div className="">
               {videogames.map((videogame) => (
-                <div key={videogame.id} className="card my-4">
-                  <div className="card-body">
-                    <div className="card-title fw-bold">{videogame.name}</div>
-                    <div className="card-text">{`${videogame.price}€`}</div>
-                    <div className="card-text">{videogame.publisher.name}</div>
-                    <div className="card-text">{videogame.genres[0].name}</div>
-                    <div className="card-text">
-                      {videogame.platforms.map((v) => (
-                        <span className="mr-2">{v.name}</span>
-                      ))}
+                <Link className="text-decoration-none" to={`/videogames/${videogame.id}`}>
+                  <div key={videogame.id} className="card my-4">
+                    <div className="card-body">
+                      <div className="card-title fw-bold">{videogame.name}</div>
+                      <div className="card-text">{`${videogame.price}`}&euro;</div>
+                      <div className="card-text">{videogame.publisher.name}</div>
+                      <div className="card-text">{videogame.genres[0].name}</div>
+                      <div className="card-text">
+                        {videogame.platforms.map((platform) => (
+                          <span className="mr-2">{platform.name} </span>
+                        ))}
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               ))}
             </div>
           </div>
         </div>
-      </div>
+      </div >
     </>
   );
 };

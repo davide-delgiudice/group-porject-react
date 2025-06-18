2804c3ee03eee56a339e979f9b13f8eff2c17524
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-18 20:15:06 +0200

    [FIX] Update Header component link class and improve text overflow handling in CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
           <div className="navbar-brand">
             <div className="d-flex align-items-center">
               <img src="./src/assets/logo.png" alt="" className="logo-design"/>
-              <Link className="nav p-2 text-white fs-3" to="/">BOOGAMING</Link>
+              <Link className="p-2 text-white fs-3" to="/">BOOGAMING</Link>
             </div>
           </div>
         </div>

c42d913c3bd81ba69dc59e4da9441185d3b007dd
brian-tura <b.tura2002@gmail.com>
2025-06-19 12:12:30 +0200

    [UPD] Added button to remove single product and correctly rounded price

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -4,31 +4,39 @@ import { useCart } from '../contexts/CartContext'
 const ShoppingCart = () => {
 
     let totalPrice = 0
+    let finalPrice = 0
 
-    const { cartItems, clearCart } = useCart()
+    const { cartItems, clearCart, loadCart } = useCart()
+
+    const removeProduct = (product) => {
+        localStorage.removeItem(`Cart-${product.id}`)
+        loadCart()
+    }
 
     for (let j = 0; j < cartItems.length; j++) {
         totalPrice += parseFloat(cartItems[j].price)
+        finalPrice = totalPrice.toFixed(2)
     }
 
-        return (
-            <>
-                <h1>Carrello</h1>
-                <div className='mb-3'>
-                    {cartItems.map((item) => (
-                        <div key={item.id}>
-                            <img src={item.image} className='img-fluid my-2' alt="" />
-                            <div className='d-flex justify-content-between'>
-                                <span>Titolo: {item.name}</span>
-                                <span>Prezzo: {item.price}&euro;</span>
-                            </div>
+    return (
+        <>
+            <h1>Carrello</h1>
+            <div className='mb-3'>
+                {cartItems.map((product) => (
+                    <div key={product.id}>
+                        <img src={product.image} className='img-fluid my-2' alt="" />
+                        <div className='d-flex justify-content-between'>
+                            <span>Titolo: {product.name}</span>
+                            <span>Prezzo: {product.price}&euro;</span>
+                            <button type='button' onClick={() => removeProduct(product)}>Rimuovi Prodotto</button>
                         </div>
-                    ))}
-                </div>
-                <h3>Spesa Totale: {totalPrice}&euro;</h3>
-                <button type='button' onClick={clearCart}>Svuota Carrello</button>
-            </>
-        )
+                    </div>
+                ))}
+            </div>
+            <h3>Spesa Totale: {finalPrice}&euro;</h3>
+            <button type='button' onClick={clearCart}>Svuota Carrello</button>
+        </>
+    )
 }
 
 export default ShoppingCart

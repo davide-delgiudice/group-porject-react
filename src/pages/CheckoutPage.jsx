eee2cef3d83cf77d128690edefdeb289f8520dad
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-22 21:15:56 +0200

    [UPD] enhance CheckoutPage to utilize loading state and improve data mapping

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 const CheckoutPage = () => {
 
-  const { savedCheckoutDatas } = useCart();
-  console.log("save-data", savedCheckoutDatas.items)
+  const { savedCheckoutDatas, isLoaded } = useCart();
+  console.log("save-data", savedCheckoutDatas);
 
   return (
     <>
@@ -24,28 +24,37 @@ const CheckoutPage = () => {
         <div className="row">
           {/* Colonna dei prodotti */}
           <div className="col-md-8">
-            {savedCheckoutDatas.items.map((data) => (
-              <div key={`data-${data.items.id}`} className="card bg-secondary p-3 mb-3 d-flex flex-row align-items-center">
-                <img src={data.items.image} className='w-25 rounded-2' alt={data.items.name} />
-                <div className='mx-3'>
-                  <h4 className='text-light fs-5 m-0'>{data.items.name}</h4>
-                  <p className='text-light m-0'>{data.items.basePrice}&euro;</p>
-                  <p className='text-light m-0'>Quantità: {data.items.quantity}</p>
+            {isLoaded &&
+              savedCheckoutDatas.items.map((data) => (
+                <div
+                  key={`data-${data.id}`}
+                  className="card bg-secondary p-3 mb-3 d-flex flex-row align-items-center"
+                >
+                  <img src={data.image} className='w-25 rounded-2' alt={data.name} />
+                  <div className='mx-3'>
+                    <h4 className='text-light fs-5 m-0'>{data.name}</h4>
+                    <p className='text-light m-0'>{data.basePrice}&euro;</p>
+                    <p className='text-light m-0'>Quantità: {data.quantity}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
 
           {/* Colonna del sommario */}
           <div className="col-md-4">
             <div className="card bg-secondary text-light p-3">
-              {savedCheckoutDatas.map((product) => (
-                <div key={`product-${product.items.id}`} className='mb-2'>
-                  <div>Prezzo: {product.items.unit_price}&euro;</div>
-                </div>
-              ))}
-              <hr />
-              {/* <h5>Totale: {product.total}&euro;</h5> */}
+              {isLoaded && (
+                <>
+                  {savedCheckoutDatas.items.map((product) => (
+                    <div key={`product-${product.id}`} className='mb-2'>
+                      <div>{product.name}</div>
+                      <div>Prezzo: {product.unit_price}&euro;</div>
+                    </div>
+                  ))}
+                  <hr />
+                  <h5>Totale: {savedCheckoutDatas.total}&euro;</h5>
+                </>
+              )}
               <div className='btn btn-danger'>Checkout{' >'}</div>
               <span className='text-center'>oppure</span>
               <Link className='text-center' to="/SearchPage">{'< '}Continua gli acquisti</Link>

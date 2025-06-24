bc28e5a69f22de619e84420e8a4273a345537655
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-24 16:47:18 +0200

    [UPD] refine Hero component layout; adjust column classes and button styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ const Hero = () => {
     <div className="herospace">
       <div className="container py-5">
         <div className="row">
-          <div className="col-6 col-override-320 col-override-375 col-override-425">
+          <div className="col-6 col-override-375 col-override-425">
             <div>
               <h2 className="fw-bold text-light">I TUOI VIDEOGIOCHI</h2>
               <h2 className="fw-bold text-light">LA TUA STORIA</h2>
               <p className="mt-3 text-light">Boogaming è dove puoi trovare i tuoi giochi preferiti, ad un prezzo vantaggioso!</p>
             </div>
-            <Link className="btn btn-color text-light fw-bold" to="/SearchPage">Dai un'occhiata</Link>
-            <Link className="btn btn-success text-light fw-bold ms-1" to="/SearchPage?offer=true">Giochi in offerta</Link>
+            <Link className="btn btn-all-color text-light fw-bold" to="/SearchPage">Dai un'occhiata</Link>
+            <Link className="btn btn-offer-color text-light fw-bold ms-1" to="/SearchPage?offer=true">Giochi in offerta</Link>
           </div>
         </div>
       </div>

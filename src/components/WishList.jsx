dd131c9f9d9692361fcdc0347cbb29cf1533b24a
davide-delgiudice <dav.del.giudice@gmail.com>
2025-06-24 16:47:02 +0200

    [UPD] enhance WishList component layout and styling; improve button interactions and responsiveness

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,63 +1,62 @@
-import React, { useState } from 'react'
-import { useWishList } from '../contexts/WishListContext'
+import React, { useState } from 'react';
+import { useWishList } from '../contexts/WishListContext';
 
 const WishList = () => {
-
-    const { wishListProducts, clearWishList, removeFromWishList } = useWishList()
-    const [showList, setShowList] = useState(false)
+    const { wishListProducts, clearWishList, removeFromWishList } = useWishList();
+    const [showList, setShowList] = useState(false);
 
     const handleClick = () => {
-        setShowList(prev => !prev)
-    }
+        setShowList(prev => !prev);
+    };
 
     return (
-        <>
-            <div>
-                <button type='button' className='btn btn-primary' onClick={handleClick}><i className="fa-solid fa-star"></i></button>
+        <div className="wishlist-wrapper">
+            <button type="button" className="btn btn-primary btn-wish" onClick={handleClick}>
+                <i className="fa-solid fa-star"></i>
+            </button>
 
-                {wishListProducts.length === 0 && showList ?
-                    (<div className='position-absolute wishlist p-3 rounded'>
-                        <h1 className='fs-3'>WishList</h1>
+            {showList && (
+                <div className="wishlist-modal rounded shadow">
+                    <div className="p-3">
+                        <h1 className="fs-3">WishList</h1>
                         <hr />
-                        <p>La tua wishlist &egrave; vuota</p>
-                    </div>) : (
-                        showList &&
-                        <div className='position-absolute wishlist rounded'>
-                            <div className='ms-2'>
-                                <h1 className='fs-3'>WishList</h1>
-                                <hr />
-                                <div className='overflow-hidden scrollable'>
-                                    <div className='overflow-y-auto h-100 overflow-x-hidden'>
-                                        {wishListProducts.map((product) => (
-                                            <div key={product.id} className='my-3'>
-                                                <div className='card cart-img'>
-                                                    <img src={product.image} className='img-fluid my-1' alt="" />
-                                                </div>
-                                                <div className='d-flex justify-content-between row'>
-                                                    <span>Titolo: {product.name}</span>
-                                                    <span>Prezzo: {product.price}&euro;</span>
-                                                </div>
-                                                <div className='d-flex align-items-center '>
-                                                    <div className='pb-2'>
-                                                        <button type='button' className='btn btn-danger' onClick={() => removeFromWishList(product)}>Rimuovi</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
+                        {wishListProducts.length === 0 ? (
+                            <p>La tua wishlist è vuota</p>
+                        ) : (
+                            <div className="wishlist-content scrollable">
+                                {wishListProducts.map(product => (
+                                    <div key={product.id} className="my-3">
+                                        <div className="card cart-img mb-2">
+                                            <img src={product.image} className="img-fluid my-1" alt={product.name} />
+                                        </div>
+                                        <div className="">
+                                            <div className='d-flex'> Titolo: {product.name}</div>
+                                            <div className='d-flex'>Prezzo: {product.price}&euro;</div>
+                                        </div>
+                                        <div className="d-flex align-items-center">
+                                            <button
+                                                type="button"
+                                                className="btn btn-wish text-light mt-2"
+                                                onClick={() => removeFromWishList(product)}
+                                            >
+                                                Rimuovi
+                                            </button>
+                                        </div>
                                     </div>
+                                ))}
+                                <hr />
+                                <div className="d-flex justify-content-center pb-3">
+                                    <button type="button" className="btn btn-wish text-light" onClick={clearWishList}>
+                                        Cancella Wishlist
+                                    </button>
                                 </div>
                             </div>
-                            <hr />
-                            <div className='p-2 d-flex justify-content-center'>
-                                <button type='button' className='btn btn-danger' onClick={clearWishList}>Cancella Wishlist</button>
-                            </div>
-                        </div>)
+                        )}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
-                }
-
-            </div>
-        </>
-    )
-}
-
-export default WishList
+export default WishList;

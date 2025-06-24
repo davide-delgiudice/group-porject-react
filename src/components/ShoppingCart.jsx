2a232383cb9111b325f006b6cef52dfbafdaca8a
brian-tura <b.tura2002@gmail.com>
2025-06-24 10:57:46 +0200

    minor fix to cart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -22,6 +22,7 @@ const ShoppingCart = () => {
                                 <div className='d-flex justify-content-between row'>
                                     <span>Titolo: {product.name}</span>
                                     <span>Prezzo: {product.price}&euro;</span>
+                                    <span>Sconto: {(product.price * product.offer).toFixed(2)}&euro;</span>
                                 </div>
                                 <div className='d-flex align-items-center '>
                                     <div className='d-flex'>
@@ -43,43 +44,16 @@ const ShoppingCart = () => {
                         ))}
                     </div>
                 </div>
-                <div className='total p-2'>
-                    <h3>Spesa Totale: {price}&euro;</h3>
-                    {cartProducts.map((product) => (
-                        <div key={product.id} className='my-3'>
-                            <img src={product.image} className='img-fluid my-1' alt="" />
-                            <div className='d-flex justify-content-between row'>
-                                <span>Titolo: {product.name}</span>
-                                <span>Prezzo: {product.price}&euro;</span>
-                                <span>Sconto: {(product.price*product.offer).toFixed(2)}&euro;</span>
-                            </div>
-                            <div className='d-flex'>
-                                <p>Quantità: </p>
-                                <div>
-                                    <button className='mx-2 btn btn-outline-secondary px-1 py-0 text-white btn-sm' type='button' onClick={() => removeFromCart(product)}>-</button>
-                                </div>
-                                <p>{product.quantity}</p>
-                                <div>
-                                    <button className='mx-2 btn btn-outline-secondary px-1 py-0 text-white btn-sm' type='button' onClick={() => addToCart(product)}>+</button>
-                                </div>
-                            </div>
-                            <div>
-                                <button type='button' className='btn btn-danger' onClick={() => removeSingleProduct(product)}>Cancella dal carrello</button>
-                            </div>
-                        </div>
-
-                    ))}
-                </div>
-            </div>
-            <div className='position-fixed bottom-0 z-3 total border-top'>
-                <div className='py-3 '>
-                    <h3>Spesa Totale: {price}&euro;</h3>
-                    <Link type='button' className='btn btn-success' to='/checkoutpage' onClick={sendCart}>Checkout</Link>
-                    <button type='button' className='btn btn-danger' onClick={clearCart}>Svuota Carrello</button>
+                <div className='position-fixed bottom-0 z-3 total border-top'>
+                    <div className='py-3 '>
+                        <h3>Spesa Totale: {price}&euro;</h3>
+                        <Link type='button' className='btn btn-success' to='/checkoutpage' onClick={sendCart}>Checkout</Link>
+                        <button type='button' className='btn btn-danger' onClick={clearCart}>Svuota Carrello</button>
+                    </div>
                 </div>
             </div>
         </>
     )
 }
 
-export default ShoppingCart
+            export default ShoppingCart
